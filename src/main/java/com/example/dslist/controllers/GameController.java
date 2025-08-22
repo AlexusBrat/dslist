b19ffaa19b39c4743dbf46b9484cf0045cabafec
AlexusBrat <alexxbrati@gmail.com>
2025-08-22 13:56:04 -0300

    GET game by id

--- src/main/java/com/example/dslist/controllers/GameController.java
+++ src/main/java/com/example/dslist/controllers/GameController.java
@@ -2,6 +2,7 @@ package com.example.dslist.controllers;
 
 import java.util.List;
 
+import com.example.dslist.dto.GameDTO;
 import com.example.dslist.dto.GameMinDTO;
 import com.example.dslist.entities.Game;
 import com.example.dslist.services.GameService;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -21,16 +23,19 @@ public class GameController {
     @Autowired
     private GameService gameService;
 
-    @GetMapping
-    public String getMethodName(@RequestParam String param) {
-        return new String();
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        GameDTO result = gameService.findById(id);
+        return result;
     }
     
+    @GetMapping
     public List<GameMinDTO> findAll() {
         List<Game> result = gameService.findAll();
         List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
         return dto;
     }
+    
 
 
     // Método para listar os dados da API fornecida pelo controller no formato DTO no protocolo HTTP, implementado para teste
--- src/main/java/com/example/dslist/services/GameService.java
+++ src/main/java/com/example/dslist/services/GameService.java
@@ -3,10 +3,12 @@ package com.example.dslist.services;
 import java.util.List;
 
 import com.example.dslist.repositories.GameRepository;
+import com.example.dslist.dto.GameDTO;
 import com.example.dslist.entities.Game;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class GameService {
@@ -15,6 +17,14 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get(); // Implementação futura: tratar ID que não existe
+        GameDTO dto = new GameDTO(result);
+        return dto;
+    }
+
+    @Transactional(readOnly = true)
     public List<Game> findAll() {
         List<Game> result = gameRepository.findAll();
         return result;
